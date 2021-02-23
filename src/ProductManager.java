eda6c41484fd9c98f9da1e490c0a9200c5006d48
Edwin <edwin.oursel@codeworks.fr>
2021-02-23 22:11:07 +0100

    answer example

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProductManager {
@@ -5,11 +7,12 @@ public class ProductManager {
     private final List<String> products;
 
     public ProductManager(List<String> products) {
+        products.sort(Comparator.comparing(String::toString));
         this.products = products;
     }
 
     public boolean productIsPresent(String product) {
-        return products.contains(product);
+        return Collections.binarySearch(products, product) < 0;
 
         // we are having a performance issue in this method
     }
